5afdc1a411997a25e78d46bbf8cea7cfa41f2d74
Naja-Naja <latimeria.h11@gmail.com>
2022-05-06 18:53:44 +0900

    リザルト画面の実装

---
+++ Assets/Project/Script/DrawChart.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XCharts;
+
+public class DrawChart : MonoBehaviour
+{
+    [SerializeField] LineChart lineChart;
+    [SerializeField] GameProperties properties;
+    //private LineChart linechart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineChart.RemoveData();
+        lineChart.AddSerie(SerieType.Line);
+        //lineChart.xAxis0,;
+        int listlength = properties.attensionLog.Count;
+        for (int i = 0; i < 60; i++)
+        {
+            if (listlength > i)
+            {
+                Debug.Log("check");
+                lineChart.AddData(0,properties.attensionLog[i]);
+
+            }
+            //lineChart.AddXAxisData(i.ToString());
+            //lineChart.AddData(0, Random.Range(0, 65));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
